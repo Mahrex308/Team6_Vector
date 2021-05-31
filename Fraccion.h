6c32f33bbee7eb902e6289863e07bbea8db91289
Marino <marinohk6@gmail.com>
2021-05-31 13:18:35 -0500

    Create Fraccion.h
    Creando un archivo Fraccion.h

---
+++ Fraccion.h
@@ -0,0 +1,20 @@
+//#pragma once
+class Fraccion{
+
+private:
+    int numerador;
+    int denominador;
+
+public:
+    Fraccion();
+    Fraccion(int, int);
+    void reduccion(int &,int &);
+    Fraccion suma(Fraccion);
+    Fraccion multiplicacion(Fraccion);
+    void imprimir();
+    int getNumerador();
+    int getDenominador();
+    void setNumerador(int);
+    void setDenominador(int);
+
+};
