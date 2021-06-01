09ce1a7d4378feb3bd09bf28a00d5ccb6585e50d
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2021-06-01 11:41:47 -0500

    Update Fraccion.h

--- Fraccion.h
+++ Fraccion.h
@@ -11,13 +11,12 @@ public:
     void reduccion(int &,int &);
     Fraccion suma(Fraccion);
     Fraccion resta(Fraccion);
-    virtual Fraccion multiplicacion(Fraccion);
+    Fraccion multiplicacion(Fraccion);
     void imprimir();
     int getNumerador();
     int getDenominador();
     void setNumerador(int);
     void setDenominador(int);
-    Fraccion multiplicacion(int);
     float decimal();
 
 };
